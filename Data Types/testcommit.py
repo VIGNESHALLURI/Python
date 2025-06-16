48c5736085991821720ba401a6748bf66150eea3
SAIVARMA01 <vigneshvarmaalluri@gmail.com>
2025-06-16 19:35:07 +0530

    test

---
+++ Data Types/testcommit.py
@@ -0,0 +1,37 @@
+# Data Types Basics in Python
+
+# Integer
+a = 10
+print("Integer:", a, type(a))
+
+# Float
+b = 3.14
+print("Float:", b, type(b))
+
+# String
+c = "Hello, World!"
+print("String:", c, type(c))
+
+# Boolean
+d = True
+print("Boolean:", d, type(d))
+
+# List
+e = [1, 2, 3, 4, 5]
+print("List:", e, type(e))
+
+# Tuple
+f = (1, 2, 3)
+print("Tuple:", f, type(f))
+
+# Set
+g = {1, 2, 3}
+print("Set:", g, type(g))
+
+# Dictionary
+h = {"name": "Alice", "age": 25}
+print("Dictionary:", h, type(h))
+
+# NoneType
+i = None
+print("NoneType:", i, type(i))
