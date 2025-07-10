79530940700f8df20707a0cf2762e73e95720059
SAIVARMA01 <vigneshvarmaalluri@gmail.com>
2025-07-10 21:38:39 +0530

    operator program3

---
+++ Data Types/Python Operators/program3.py
@@ -0,0 +1,8 @@
+a = 20
+b = 12
+print("Sum:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Quotient:", a / b)
+print("Floor Division:", a // b)
+print("Remainder:", a % b)
