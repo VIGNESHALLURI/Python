4fb525687b9932e97ee5f83d36c49be4b693ce0d
SAIVARMA01 <vigneshvarmaalluri@gmail.com>
2025-07-13 12:30:15 +0530

    operator program3

---
+++ Operators/python3.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+print("Addition:", a + b)
+print("Subtraction:", b - a)
+print("Multiplication:", a * b)
+print("Division:", b / a)
+print("Floor Division:", b // a)
+print("Modulus:", b % a)
+print("Exponentiation:", a ** 2)
