61eb850b269c25c949afec272f34cc6b7f8cc4d3
Vignesh Varma <allurivignesh02@gmail.com>
2025-07-14 11:46:32 +0530

    Update Program5.py

--- Program5.py
+++ Program5.py
@@ -1,3 +1,3 @@
-#Write a program to evaluate the expression  (4 != 2) or (4 > 5) or (4 < 5) using arithmetic operators.
+#Write a program to evaluate the expression  (4 != 2) or (4 > 5) or (4 < 5) using logical operators.
 result = (4 != 2) or (4 > 5) or (4 < 5)
 print("result:", result)
